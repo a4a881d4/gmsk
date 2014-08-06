316448740f93d49b8e40c58ba291057ef9814656
a4a881d4 <a4a881d4@163.com>
2014-08-06 23:31:40 +0800

    0.4

--- python/GMSKBB.py
+++ python/GMSKBB.py
@@ -1,5 +1,6 @@
 import constants
 import Qfunc
+import random
 
 class GMSKBB:
 	def __init__(self,f,Fs):
@@ -16,12 +17,13 @@ class GMSKBB:
 		return self.over
 	
 	def report(self):
-		print "#",len(self.State),self.HState,self.over,self.State[0],self.AS
+		print "#",self.over,len(self.State),self.HState,"|",self.State[0],self.State[1],self.State[2],self.State[3],"|",self.Table[0*constants.GMSKOSCF+self.AS],self.Table[1*constants.GMSKOSCF+self.AS],self.Table[2*constants.GMSKOSCF+self.AS],self.Table[3*constants.GMSKOSCF+self.AS],self.AS
 					
 	def ce( self,inS ):
+		# self.report()
 		if self.over==1:
 			self.over=0
-			last = self.State.pop()
+			last = self.State.pop(0)
 			if last == -1:
 				self.HState = (self.HState-1)&3
 			elif last == 1:
@@ -31,22 +33,24 @@ class GMSKBB:
 			else:
 				self.State.append(-1)
 				
-		ret = constants.GMSKP*(self.HState+4)
+		ret = constants.GMSKP*self.HState
 		for T in range(-constants.GMSKRange,constants.GMSKRange):
 			t = T + constants.GMSKRange
-			ret = ret + self.Table[t*constants.GMSKOSCF+self.AS]*self.State[-t]
+			ret = ret + self.Table[t*constants.GMSKOSCF+self.AS]*self.State[constants.GMSKRange*2-1-t]
 				
 		self.AS = self.AS + self.Acc
 		if self.AS>=constants.GMSKOSCF:
 			self.over=1
 		self.AS = self.AS%constants.GMSKOSCF		
-		self.report()
 		
 		return ret%(4*constants.GMSKP)
 		
 if __name__ == '__main__':
 	gmsk = GMSKBB(5.2e6,192e6)
-	data = [0,1,0,1,0,1,1,0,0,1,1,1,1,1]
+	data = []
+	for i in range(0,384):
+		d = random.randint(0,1)
+		data.append(d)
 	
 	while data:
 		phase=0
